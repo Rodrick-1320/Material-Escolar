1598ae16b0bee2a7b5141fb1bcf08040d1d11002
Rodrigo Silva <rodrigo.silva@catskillet.com>
2022-10-25 16:31:10 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style.css">
+
+	<title></title>
+</head>
+<body>
+
+<! -- Header-->
+
+	<header>
+		 
+		 <div class="head">
+		 	<h1>OP Papelaria</h1>
+		 </div>
+
+	</header>
+
+		<div class="head2">
+			<h3>Entre nosso catalogo temos: </h3>
+			<h4>
+			<a href="#cadernos" >Cadernos</a> | <a href="#canetas">Canetas</a>  |
+			<a href="#lapis" >Lápis</a> | <a href="#aderecos">Adereços</a>
+
+			</h4>
+		</div>
+
+<! -- Seção 1 / Cadernos -->
+
+	<a name="cadernos">
+
+	<div class="sec">
+		<h2>CADERNOS </h2>
+	</div>
+
+<form method="post" action="">
+
+	<table border="1" >
+		<tr>
+			<td><img src="imagens/cdr1.png"></td> <td><img src="imagens/cdr2.png"></td> 
+			<td><img src="imagens/cdr3.png"></td> <td><img src="imagens/cdr4.png"></td>
+
+		</tr>
+
+		<tr>
+			<td>
+				<div class="txtprdt">CAPA DURA A4 SKATE - 10 MATERIAS</div> 
+				<div class="prc"> R$ 27,90 </div>
+				Quantidade: <input type="number" name="cdr1">
+				<button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt">CAPA DURA A4 LEGEND - 15 MATERIAS </div> 
+				<div class="prc"> R$ 35,60 </div>
+				Quantidade: <input type="number" name="cdr2"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt">CAPA DURA A4 PRIMAVERA - 10 MATERIAS </div>
+				<div class="prc"> R$ 27,90 </div> 
+				Quantidade: <input type="number" name="cdr3"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> SEM PAUTA  - 1 MATERIA</div>
+				<div class="prc"> R$ 13,50 </div> 
+				Quantidade: <input type="number" name="cdr4"> <button>Comprar</button>
+			</td> 
+
+		</tr>
+		
+	</table>
+
+	</a>
+<! -- Seção 2 / Canetas -->
+
+	<a name="canetas">
+
+	<div class="sec">
+		<h2>CANETAS </h2>
+	</div>
+
+	<table border="1" >
+		<tr>
+			<td><img src="imagens/cnt1.png"></td> <td><img src="imagens/cnt2.png"></td> 
+			<td><img src="imagens/cnt3.png"></td> <td><img src="imagens/cnt4.png"></td>
+
+		</tr>
+
+		<tr>
+			<td>
+				<div class="txtprdt"> CANETA AZUL - PONTA GROSSA</div>
+				<div class="prc"> R$ 2,50 </div>
+				Quantidade: <input type="number" name="cnt1"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> CANETA VERMELHA - PONTA GROSSA</div>
+				<div class="prc"> R$ 2,50 </div>
+				Quantidade: <input type="number" name="cnt2"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> CANETA 4 CORES - TILIBRA</div>
+				<div class="prc"> R$ 7,90 </div>
+				Quantidade: <input type="number" name="cnt3"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt">CANETA ROXA - PONTA FINA</div>
+				<div class="prc"> R$ 3,50 </div>
+				Quantidade: <input type="number" name="cnt4"> <button>Comprar</button>
+			</td> 
+
+		</tr>
+
+	</table>
+
+
+	</a>
+
+<! -- Seção 3 / Lápis -->
+
+	<a name="lapis">
+
+	<div class="sec">
+		<h2>LÁPIS / LAPISEIRA /  BORRACHA </h2>
+	</div>
+
+	<table border="1" >
+		<tr>
+			<td><img src="imagens/lps1.png"></td> <td><img src="imagens/lps2.png"></td> 
+			<td><img src="imagens/lps3.png"></td> <td><img src="imagens/lps4.png"></td>
+
+		</tr>
+
+		<tr>
+
+			<td>
+				<div class="txtprdt">LÁPIS SIMPLES</div>
+				<div class="prc"> R$ 1,90 </div>
+				Quantidade: <input type="number" name="lps1"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> LÁPIS DE COR - 24 CORES</div>
+				<div class="prc"> R$ 22,30 </div>
+				Quantidade: <input type="number" name="lps2"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> LAPISEIRA METALICA 0.7</div>
+				<div class="prc"> R$ 7,80 </div>
+				Quantidade: <input type="number" name="lps3"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> BORRACHA BRANCA G</div>
+				<div class="prc"> R$ 2,00 </div>
+				Quantidade: <input type="number" name="lps4"> <button>Comprar</button>
+			</td> 
+
+		</tr>
+		
+	</table>
+	
+	</a>
+
+
+<! -- Seção 4 / Adereços -->
+
+	<a name="aderecos">
+
+	<div class="sec">
+		<h2>ADEREÇOS </h2>
+	</div>
+
+	<table border="1" >
+		<tr>
+			<td><img src="imagens/adr1.png"></td> <td><img src="imagens/adr2.png"></td> 
+			<td><img src="imagens/adr3.png"></td> <td><img src="imagens/adr4.png"></td>
+
+		</tr>
+
+		<tr>
+			<td>
+				<div class="txtprdt"> PASTA DE DOCUMENTOS - CINZA</div>
+				<div class="prc"> R$ 8,30 </div>
+				Quantidade: <input type="number" name="adr1"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> TIL-NOTES - 3 BLOCOS</div>
+				<div class="prc"> R$ 9,50 </div>
+				Quantidade: <input type="number" name="adr2"> <button>Comprar</button>
+			</td> 
+
+			<td>
+				<div class="txtprdt"> BLOCO DE ANOTAÇÕES</div>
+				<div class="prc"> R$ 5,50 </div>
+				Quantidade: <input type="number" name="adr3"> <button>Comprar</button>
+			</td>
+
+			<td>
+				<div class="txtprdt"> CALCULADORA - 12 DIGITOS </div>
+				<div class="prc"> R$ 12,50 </div>
+				Quantidade: <input type="number" name="adr4"> <button>Comprar</button>
+			</td> 
+			
+		</tr>
+		
+	</table>
+		<center><br>Nome do Comprador: </center>
+		<center><input type="text" name="nome" placeholder="DIGITE SEU NOME: "></center>
+		<center><input type="submit" value="IMPRIMIR E ADICIONAR AO BDD" name="botao" ></center>	
+
+
+</form>
+
+	</a>
+
+</body>
+</html>
+
+
+<?php
+	 
+	if(isset($_POST["botao"])){
+
+	echo "<br><br><center><h1>RELATÓRIO DE COMPRAS: </h1></center><br><br>";
+
+	@$nome = $_POST['nome'];
+	@$data = date('d/m/y');
+
+    @$cdr1 = $_POST['cdr1']; $vcdr1 = intval($cdr1) * 27.90; // caderno 1 
+	@$cdr2 = $_POST['cdr2']; $vcdr2 = intval($cdr2) * 35.60; // caderno 2
+	@$cdr3 = $_POST['cdr2']; $vcdr3 = intval($cdr3) * 27.90; // caderno 3
+	@$cdr4 = $_POST['cdr4']; $vcdr4 = intval($cdr4) * 13.50; // caderno 4
+
+	@$cnt1 = $_POST['cnt1']; $vcnt1 = intval($cnt1) * 2.50; // caneta 1
+	@$cnt2 = $_POST['cnt2']; $vcnt2 = intval($cnt2) * 2.50; // caneta 2
+	@$cnt3 = $_POST['cnt3']; $vcnt3 = intval($cnt3) * 7.90; // caneta 3
+	@$cnt4 = $_POST['cnt4']; $vcnt4 = intval($cnt4) * 3.50; // caneta 4
+
+	@$lps1 = $_POST['lps1']; $vlps1 = intval($lps1)  * 1.90; // lapis 1
+	@$lps2 = $_POST['lps2']; $vlps2 = intval($lps2)  * 22.30; // lapis 2
+	@$lps3 = $_POST['lps3']; $vlps3 = intval($lps3)  * 7.80; // lapis 3
+	@$lps4 = $_POST['lps4']; $vlps4 = intval($lps4)  * 2.00; // lapis 4
+
+	@$adr1 = $_POST['adr1']; $vadr1 = intval($adr1) * 8.30; // adereço 1
+	@$adr2 = $_POST['adr2']; $vadr2 = intval($adr2) * 9.50; // adereço 2
+	@$adr3 = $_POST['adr3']; $vadr3 = intval($adr3) * 5.50; // adereço 3
+	@$adr4 = $_POST['adr4']; $vadr4 = intval($adr4) * 12.50; // adereço 4
+
+	$vt = $vcdr1 + $vcdr2 + $vcdr3 + $vcdr4 + $vcnt1 + $vcnt2 + $vcnt3 + $vcnt4 + $vlps1 +  $vlps2 + $vlps3 + $vlps4+ $vadr1 + $vadr2 + $vadr3 + $vadr4 ;
+
+	echo "<table style='border: 1px solid royalblue;'>";
+
+	echo "<tr> <td>NOME DO COMPRADOR:  </td> <td> ".$nome."</td> <td>DATA DA COMPRA: </td> <td>".$data."</td> </tr>";
+	echo "<tr> <td>-</td> <td>-</td> <td>-</td> <td>-</td> </tr>";
+
+	
+	// Cadernos
+	echo "<tr> <td>PRODUTOS </td> <td>QUANTIDADE</td> <td>VALOR UNITARIO</td> <td>VALOR TOTAL</td> </tr>";
+	if($cdr1  >= 1 ){echo "<tr> <td>CAPA DURA A4 SKATE - 10 MATERIAS </td> <td>" .$cdr1 . "</td> <td>R$ 27,90 </td> <td> R$ " . $vcdr1 . "</td> </tr>";}
+	if($cdr2  >= 1 ){echo "<tr> <td>CAPA DURA A4 LEGEND - 15 MATERIAS </td> <td>" .$cdr2 . "</td> <td>R$ 35,60 </td> <td> R$ " . $vcdr2 . "</td> </tr>";}
+	if($cdr3  >= 1 ){echo "<tr> <td>CAPA DURA A4 PRIMAVERA - 10 MATERIAS </td> <td>" .$cdr3 . "</td> <td>R$ 27,90 </td> <td> R$ " . $vcdr3 . "</td> </tr>";}
+	if($cdr4  >= 1 ){echo "<tr> <td>SEM PAUTA - 1 MATERIA </td> <td>" .$cdr4 . "</td> <td>R$ 13,50 </td> <td> R$ " . $vcdr4 . "</td> </tr>";}
+	// canetas
+	if($cnt1  >= 1 ){echo "<tr> <td>CANETA AZUL - PONTA GROSSA </td> <td>" .$cnt1 . "</td> <td>R$ 2,50 </td> <td> R$ " . $vcnt1 . "</td> </tr>";}
+	if($cnt2  >= 1 ){echo "<tr> <td>CANETA VERMELHA - PONTA GROSSA</td> <td>" .$cnt2 . "</td> <td>R$ 2,50 </td> <td> R$ " . $vcnt2 . "</td> </tr>";}
+	if($cnt3  >= 1 ){echo "<tr> <td>CANETA 4 CORES - TILIBRA</td> <td>" .$cnt3 . "</td> <td>R$ 7,90 </td> <td> R$ " . $vcnt3 . "</td> </tr>";}
+	if($cnt4  >= 1 ){echo "<tr> <td>CANETA ROXA - PONTA FINA</td> <td>" .$cnt4 . "</td> <td>R$ 3,50 </td> <td> R$ " . $vcnt4 . "</td> </tr>";}
+	// lapis
+	if($lps1  >= 1 ){echo "<tr> <td>LÁPIS SIMPLES </td> <td>" .$lps1 . "</td> <td>R$ 1,90 </td> <td> R$ " . $vlps1 . "</td> </tr>";}
+	if($lps2  >= 1 ){echo "<tr> <td>LÁPIS DE COR - 24 CORES</td><td>" .$lps2 . "</td> <td>R$ 22,30 </td> <td> R$ " . $vlps2 . "</td> </tr>";}
+	if($lps3  >= 1 ){echo "<tr> <td>LAPISEIRA METALICA 0.7</td> <td>" .$lps3 . "</td> <td>R$ 7,80 </td> <td>R$ " . $vlps3 . "</td> </tr>";}
+	if($lps4  >= 1 ){echo "<tr> <td>BORRACHA BRANCA G</td> <td>" .$lps4 . "</td> <td>R$ 2,00 </td> <td> R$ " . $vlps4 . "</td> </tr>";}
+	// adereços
+	if($adr1  >= 1 ){echo "<tr> <td>PASTA DE DOCUMENTOS - CINZA </td> <td>" .$adr1 . "</td> <td>R$ 8,30 </td> <td> R$" . $vadr1 . "</td> </tr>";}
+	if($adr2  >= 1 ){echo "<tr> <td>TIL-NOTES - 3 BLOCOS </td> <td>" .$adr2 . "</td> <td>R$ 9,50 </td> <td>R$ " . $vadr2 . "</td> </tr>";}
+	if($adr3  >= 1 ){echo "<tr> <td>BLOCO DE ANOTAÇÕES </td> <td>" .$adr3 . "</td> <td>R$ 5,50 </td> <td> R$ " . $vadr3 . "</td> </tr>";}
+	if($adr4  >= 1 ){echo "<tr> <td>CALCULADORA - 12 DIGITOS </td> <td>" .$adr4 . "</td> <td>R$ 12,50 </td> <td>R$ " . $vadr4 . " </td> </tr>";}
+
+	echo "<tr>  <td style='color:red;'> VALOR TOTAL: </td> <td style='color:red;'>-</td> <td style='color:red;'>-</td> <td style='color:red;'> R$ " . $vt . "</td> </tr>";
+
+
+	echo "</table>";
+
+	
+}
+
+	$servername = "localhost";
+	$database= "materialescolar";
+	$passw = "";
+	$username ="root";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $database);
+
+	// Check connection
+	if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+	}
+ 
+	echo "Connected successfully";
+	 
+	$sql = "INSERT INTO Students (name, lastname, email) VALUES ('Test', 'Testing', 'Testing@tesing.com')";
+
+	if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+	} 
+	else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+
+	mysqli_close($conn);
+?>
